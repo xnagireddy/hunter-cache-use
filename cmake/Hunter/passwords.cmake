0e700fe6ca22826e5409ebf999aa6dd1364ed4c3
X Nagireddy <xnagireddy@gmail.com>
2018-10-16 18:07:35 +0530

    Updated the configuration to trigger a build and upload to the hunter cache.

--- cmake/Hunter/passwords.cmake
+++ cmake/Hunter/passwords.cmake
@@ -1,10 +1,10 @@
 hunter_upload_password(
     # REPO_OWNER + REPO = https://github.com/forexample/hunter-cache
-    REPO_OWNER "forexample"
+    REPO_OWNER "xnagireddy"
     REPO "hunter-cache"
 
-    # USERNAME = https://github.com/ingenue
-    USERNAME "ingenue"
+    # USERNAME = https://github.com/xagnot
+    USERNAME "xagnot"
 
     # PASSWORD = GitHub token saved as a secure environment variable
     PASSWORD "$ENV{GITHUB_USER_PASSWORD}"
