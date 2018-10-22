4305bd8f1b5b94099a03c08f62613a119a445435
X Nagireddy <xnagireddy@gmail.com>
2018-10-22 19:20:13 +0530

    Updated to support artifactory as hunter binary cache server.

--- cmake/Hunter/passwords.cmake
+++ cmake/Hunter/passwords.cmake
@@ -1,11 +1,18 @@
-hunter_upload_password(
-    # REPO_OWNER + REPO = https://github.com/forexample/hunter-cache
-    REPO_OWNER "xnagireddy"
-    REPO "hunter-cache"
+set(artifactory_reader_key "AKCp5bCBKXM8QXopQPoPshC1PfqJDBtRC4Yhf82bKZ6cgEB9RJgBHS6ZA7bkgQjChS7XG64x9")
+set(artifactory_writer_key "AKCp5bCBKXM8RxibkzgP43Pc5ss3zCoBinUEbpKTqVdWy4hAHWvsJ6DEvDhg9TF4goq3a6wQa")
 
-    # USERNAME = https://github.com/xagnot
-    USERNAME "xagnot"
+set(server "http://localhost:8081/artifactory/hunter")
 
-    # PASSWORD = GitHub token saved as a secure environment variable
-    PASSWORD "$ENV{GITHUB_USER_PASSWORD}"
+hunter_cache_server_password(
+    SERVER "${server}"
+    HTTPHEADER "X-JFrog-Art-Api: ${artifactory_reader_key}"
+    SUB_SHA1_SUFFIX
 )
+
+hunter_upload_password(
+    SERVER "${server}"
+    HTTPHEADER "X-JFrog-Art-Api: ${artifactory_writer_key}"
+    SUB_SHA1_SUFFIX
+)
+
+
